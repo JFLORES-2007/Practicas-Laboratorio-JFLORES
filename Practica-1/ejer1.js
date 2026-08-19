a0ab68cd6275953ce0b2c6bee6c78c57730e3680
Jorge Flores <jorgeluflores.2007@gmail.com>
2026-08-19 14:28:37 -0600

    EjerciciosDePractica-01

---
+++ Practica-1/ejer1.js
@@ -0,0 +1,17 @@
+// Ejercicio 1: Operaciones Aritméticas Múltiples
+// Declare dos variables numéricas. Realice todas las operaciones aritméticas básicas
+// (suma, resta, multiplicación, división y módulo) y muestre cada resultado por separado en
+// consola
+
+let num1 = 10;
+let num2 = 20;
+
+let suma = num1 + num2;
+let resta = num1 - num2;
+let multiplicacion = num1 * num2;
+let modulo = num2%num1;
+
+console.log(`la suma es: `+suma)
+console.log(`la resta es: `+resta)
+console.log(`la multiplicacion es: `+multiplicacion)
+console.log(`la modulo es: `+modulo)
