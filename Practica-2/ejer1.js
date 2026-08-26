3e134a9e19bc74e0f0316892f984bb3fdbf7d6dd
Jorge Flores <jorgeluflores.2007@gmail.com>
2026-08-26 13:26:51 -0600

    EjerciciosPractica-02

---
+++ Practica-2/ejer1.js
@@ -0,0 +1,9 @@
+// Transformación de texto: Solicite al usuario su nombre. Muestre el nombre en mayúsculas y en minúsculas.
+
+let nombre =`Jorge`
+
+let nombreMin = nombre.toLowerCase()
+let nombreMayus = nombre.toUpperCase()
+
+console.log("Su nombre en minuscula es: "+nombreMin);
+console.log("Su nombre en mayuscula es: "+nombreMayus);
